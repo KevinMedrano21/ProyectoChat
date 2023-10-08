23251ebd48344f75ab03cde6fba1a88f5d8da152
kevinmedrano21 <kmedrano433@gmail.com>
2023-10-07 21:38:27 -0600

    correccion del campo usuario del login

--- client/src/routers/index.js
+++ client/src/routers/index.js
@@ -10,13 +10,39 @@ const servidor ={
 
 
 
-
+//ruta pra registro
 router.post('/add', async(req, res)=>{
     const info = new Usuarios(req.body);
     console.log(req.body);
     await info.save();
     res.redirect('/');
-})
+});
+
+//ruta para inicio de sesion
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    
+    try {
+      // Busca un usuario con el correo electrónico proporcionado
+    const user = await Usuario.findOne({ mail: email });
+
+      // Si no se encuentra el usuario, redirige de nuevo al formulario de inicio de sesión
+    if (!user) {
+        return res.redirect('/login');
+    }
+
+      // Verifica la contraseña
+    if (user.psw !== password) {
+        return res.redirect('/login');
+    }
+
+      // Si las credenciales son válidas, puedes redirigir al usuario a la página de chat o realizar otras acciones
+    res.redirect('/chat');
+    } catch (error) {
+    console.error(error);
+    res.status(500).send('Error interno del servidor');
+    }
+});
 
 
 const client = net.createConnection(servidor);
